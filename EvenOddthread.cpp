03675a1da9b80ff4777ab75563fbe43151740acf
Kirankumar-m-training <136049999+Kirankumar-m-training@users.noreply.github.com>
2023-06-16 10:36:03 +0530

    Simple c++ program to print even and odd numbers using multithreading.

---
+++ EvenOddthread.cpp
@@ -0,0 +1,36 @@
+// Simple c++ program to print even and odd numbers using multithreading.
+#include <iostream>
+#include <thread>
+
+using namespace std;
+
+void oddNumbers(int n)
+{
+   for(int i=1; i<=n;i++){
+     if(i%2 != 0){
+       cout<<"Odd Number: "<<i<<endl;
+     }
+   }
+}
+
+void evenNumbers(int n)
+{
+for(int i=1; i<=n;i++){
+     if(i%2 == 0){
+       cout<<"Even Number: "<<i<<endl;
+     }
+   }
+}
+
+
+int main()
+{
+    int n;
+    cout<<"Enter how many number you want to clasify even and odd from 1 to n"<<endl;
+    cin>>n;
+    thread t1(evenNumbers, n);
+    thread t2(oddNumbers, n);
+    t1.join();
+    t2.join();
+    return 0;
+}
