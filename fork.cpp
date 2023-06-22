1adb6e54383581c07a6a634fa59c8c1d4737c101
Kirankumar-m-training <136049999+Kirankumar-m-training@users.noreply.github.com>
2023-06-22 10:12:43 +0530

    Program to demonstrate fork system call.

---
+++ fork.cpp
@@ -0,0 +1,27 @@
+// C++ program to demonstrate fork system call.
+
+include<iostream>
+#include<unistd.h>
+#include<sys/types.h>
+#include<sys/wait.h>
+
+
+int main()
+{
+pid_t p;
+std::cout<<"before fork\n";
+p=fork();
+if(p==0)   // child process
+{
+        sleep(5); // execute orphon process
+        std::cout<<"I am child having id:"<<getpid()<<std::endl;
+        std::cout<<"My parent's id is :"<<getppid()<<std::endl;
+}
+else{  // parent process
+        wait(NULL); //to make child execute first
+        std::cout<<"My child's id is:"<<p<<std::endl;
+        std::cout<<"I am parent having id:"<<getpid()<<std::endl;
+}
+  printf("Hello\n");
+  return 0;
+}
